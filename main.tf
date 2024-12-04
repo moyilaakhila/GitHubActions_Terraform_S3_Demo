729cb9e9f9f7cc21ecc385e744553b9e1caae813
moyilaakhila <103638862+moyilaakhila@users.noreply.github.com>
2024-12-04 11:24:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "aws_s3_bucket" "bucket100" {
 	bucket = "akhila-100-bucket"
 }
 
-resource "aws_s3_bucket" "bucket2" {
-    
+resource "aws_s3_bucket" "bucket200" {
+        bucket = "akhila-200-bucket"  # You need to specify a unique bucket name here 
 }
 
 provider "aws" {
